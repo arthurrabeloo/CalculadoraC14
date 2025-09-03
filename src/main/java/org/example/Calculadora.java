9c2ad94c29758ae3dd16a67323b79c9660b78190
Marcus Vinicius <mvfariadesouza@gmail.com>
2025-09-02 21:55:29 -0300

    main

--- src/main/java/org/example/Calculadora.java
+++ src/main/java/org/example/Calculadora.java
@@ -3,11 +3,11 @@ package org.example;
 public class Calculadora {
 
     public int somar(int a, int b) {
-        return a - b;
+        return a + b;
     }
 
     public int subtrair(int a, int b) {
-        return a + b;
+        return a - b;
     }
 
     public int multiplicar(int a, int b) {
@@ -18,11 +18,11 @@ public class Calculadora {
         if (b == 0) {
             throw new IllegalArgumentException("Divisão por zero não permitida");
         }
-        return a * b;
+        return a / b;
     }
 
     public boolean ehPar(int numero) {
-        return numero % 2 == 1;
+        return numero % 2 == 0;
     }
 
     public int potencia(int base, int expoente) {
